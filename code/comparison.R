d66d53a19ece62230600aec9abdfe210f8a6ffe5
vjvelascorios <vj.velascorios@gmail.com>
2024-05-04 22:18:20 -0600

    reports
    excel reports with deleted and added packages

--- code/comparison.R
+++ code/comparison.R
@@ -1,54 +1,107 @@
+#==============================================================================
+# Script para comparar los paquetes de CRAN de esta semana con la semana anterior
+#==============================================================================
+# Autor: vjvelascorios
+# Fecha: 2024-05-04
+#==============================================================================
+
+# Limpiar entorno
+rm(list = ls())
+
 # Cargar los paquetes necesarios
-library(dplyr)
-
-# Obtener la fecha actual
-fecha_actual <- Sys.Date()
-
-# Obtener la fecha de la semana anterior
-fecha_semana_anterior <- fecha_actual - 7
+pacman::p_load(dplyr, readr, tictoc)
 
 # Construir las rutas a los archivos de esta semana y la semana anterior
-ruta_archivo_actual <- paste0("/home/vjvelascorios/Documentos/weekly cran packages/data/", fecha_actual, " weekly cran package table.csv")
-ruta_archivo_semana_anterior <- paste0("/home/vjvelascorios/Documentos/weekly cran packages/data/", fecha_semana_anterior, " weekly cran package table.csv")
+setwd_auto <- function() {
+  if (Sys.info()['sysname'] == 'Windows') {
+    setwd("C:/Users/vjvelascorios/Documents/weekly-cran-packages")
+  } else {
+    setwd("/home/vjvelascorios/Documentos/weekly cran packages/data")
+  }
+}
+setwd_auto()
 
-# Verificar si los archivos existen antes de cargarlos en data frames
-if (file.exists(ruta_archivo_actual) && file.exists(ruta_archivo_semana_anterior)) {
-  df_actual <- read.csv(ruta_archivo_actual)
-  df_anterior <- read.csv(ruta_archivo_semana_anterior)
+dir = getwd()
+
+# Lista completa de archivos en el directorio de datos
+files = list.files(paste0(dir,"/data"))
+
+# filtrar solo archivos csv en files y ordenarlos
+files = files[grep(".csv", files)] %>% sort()
+
+# generar algoritmo para generar cargar archivos y compararlos y generar reporte de comparación entre elementos nuevos, eliminados y actualizados de los paquetes de CRAN
+
+
+#==============================================================================
+#==============================================================================
+# previous_file <- read_csv(paste0(dir,"/data/",files[76]))
+# actual_file <- read_csv(paste0(dir,"/data/",files[77]))
+# 
+# 
+# # Identificar los paquetes eliminados
+# paquetes_eliminados <- setdiff(previous_file$package, actual_file$package)
+# 
+# # Identificar los paquetes agregados
+# paquetes_agregados <- setdiff(actual_file$package, previous_file$package)
+# 
+# # # Identificar los paquetes actualizados
+# # paquetes_actualizados <- intersect(previous_file$package, actual_file$package)
+# 
+# 
+# # Filtrar los datos de los paquetes eliminados, agregados y actualizados en ambos data frames y guardarlos en un excel.
+# 
+# df_paquetes_eliminados <- previous_file %>%
+#   filter(package %in% paquetes_eliminados)
+# 
+# df_paquetes_agregados <- actual_file %>%
+#   filter(package %in% paquetes_agregados)
+# 
+# 
+# df_repor <- list("eliminados"=df_paquetes_eliminados, "agregados"=df_paquetes_agregados)
+# 
+# # obtener como texto de la fecha del último dataframe para guardarlo en el nombre del reporte:
+# elemento <- pull(actual_file[1, 1])
+# name_report <- list(as.character(elemento))
+# getwd()
+# 
+# write.xlsx(df_repor,file = paste0("reports/",name_report,".xlsx"), overwrite = T)
+#==============================================================================
+#==============================================================================
+
+
+tic()
+for (i in 1:length(files)) {
+  previous_file <- read_csv(paste0(dir,"/data/",files[i]))
+  actual_file <- read_csv(paste0(dir,"/data/",files[i+1]))
+  
   
   # Identificar los paquetes eliminados
-  paquetes_eliminados <- setdiff(df_anterior$package, df_actual$package)
+  paquetes_eliminados <- setdiff(previous_file$package, actual_file$package)
   
   # Identificar los paquetes agregados
-  paquetes_agregados <- setdiff(df_actual$package, df_anterior$package)
+  paquetes_agregados <- setdiff(actual_file$package, previous_file$package)
   
-  # Identificar los paquetes actualizados
-  paquetes_actualizados <- intersect(df_anterior$package, df_actual$package)
+  # # Identificar los paquetes actualizados
+  # paquetes_actualizados <- intersect(previous_file$package, actual_file$package)
   
-  # Filtrar los datos de los paquetes actualizados en ambos data frames
-  df_paquetes_actualizados_anterior <- df_anterior %>%
-    filter(package %in% paquetes_actualizados)
   
-  df_paquetes_actualizados_actual <- df_actual %>%
-    filter(package %in% paquetes_actualizados)
+  # Filtrar los datos de los paquetes eliminados, agregados y actualizados en ambos data frames y guardarlos en un excel.
   
-  # Combinar los data frames para comparar los detalles de los paquetes actualizados
-  df_comparacion <- full_join(df_paquetes_actualizados_anterior, df_paquetes_actualizados_actual, by = "package", suffix = c("_anterior", "_actual"))
+  df_paquetes_eliminados <- previous_file %>%
+    filter(package %in% paquetes_eliminados)
   
-  # Filtrar los registros donde hay cambios en los detalles de los paquetes actualizados
-  df_actualizaciones_diferentes <- df_comparacion %>%
-    filter(title_anterior != title_actual | link_anterior != link_actual | `package manual_anterior` != `package manual_actual`)
+  df_paquetes_agregados <- actual_file %>%
+    filter(package %in% paquetes_agregados)
   
-  # Ahora puedes guardar los resultados en un nuevo archivo CSV para su análisis o uso posterior
-  ruta_resultado <- "/home/vjvelascorios/Documentos/weekly cran packages/reports/resultados.csv"
   
-  write.csv(df_actualizaciones_diferentes, file = ruta_resultado, row.names = FALSE)
+  df_repor <- list("eliminados"=df_paquetes_eliminados, "agregados"=df_paquetes_agregados)
   
-  # Imprimir los resultados para verlos en la consola
-  print(paquetes_eliminados)
-  print(paquetes_agregados)
-  print(df_actualizaciones_diferentes)
-} else {
-  print("No se encontraron los archivos de esta semana y la semana anterior.")
+  # obtener como texto de la fecha del último dataframe para guardarlo en el nombre del reporte:
+  elemento <- pull(actual_file[1, 1])
+  name_report <- list(as.character(elemento))
+  getwd()
+  
+  write.xlsx(df_repor,file = paste0("reports/",name_report,".xlsx"), overwrite = T)
+  
 }
-
+toc()
